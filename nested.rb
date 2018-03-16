704dc694461e2752de348500bf979963837216ad
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-16 23:22:01 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -136,6 +136,7 @@ def adding_to_dennis
 	# attempting secondary method...
 	new_method = programmer_hash[:dennis_ritchie]
 	new_method_tier2 = new_method[:languages]
+	binding.pry
 	new_method_tier2 << "Assembly"
 
 end
